5715ada9f63aa2ecdad0dab25198027cbba767ed
LucasRP <rodrigues.lrp21@gmail.com>
2025-07-19 18:36:39 -0300

    feat: funcao de valor tem q assumir uma posicao no tabuleiro

--- 8puzzle.py
+++ 8puzzle.py
@@ -1,13 +1,25 @@
 from pysat.solvers import Glucose3
 
-#deixando os tamanhos maximos de tabuleiro e do valor usado
+#deixando os tamanhos maximos de tabuleiro, do valor usado e movimentações
 VALOR_MAXIMO = 9
 TAMANHO_MAXIMO = 3
+ADJACENCIAS = {
+  1: [2,4],
+  2: [1,3,5],
+  3: [2,6],
+  4: [1,5,7],
+  5: [2,4,6,8],
+  6: [3,5,9],
+  7: [4,8],
+  8: [5,7,9],
+  9: [6,8]
+}
 
-#instancia o glucose(vazio)
+# instancia o glucose(vazio)
 g = Glucose3()
 
 
+# adiciona a clasula ao glucose
 def addClasulasEmGlucose(lista):
    contador = 0
    for clasula in lista:
@@ -15,6 +27,7 @@ def addClasulasEmGlucose(lista):
       g.add_clause(clasula)
       
 
+# gera as clasulas e armazena em um dicionario
 def gerarDictClasulaVariavel(passo):
     auxDicionario = {}
     contador = 1
@@ -30,7 +43,7 @@ def gerarDictClasulaVariavel(passo):
         
     return auxDicionario
 
-
+#  funcao para definir que cada quadradinho tem que assumir um valor ao menos
 def gerarListaClasulasPeloMenosUm(dicionarioClasulas):
    listaClasulas = []
    for i in range(TAMANHO_MAXIMO):
@@ -44,12 +57,7 @@ def gerarListaClasulasPeloMenosUm(dicionarioClasulas):
             
    return listaClasulas
 
-
-
-
-
-
-#arrumar o de ser p ^ -p 
+# funcao para definir que so pode ter um valor em cada quadrado
 def gerarListaClasulasSoPodeUm(clasulas):
     listaNovasClasulas = []
     tamanhoClasula = len(clasulas[0])
@@ -64,11 +72,30 @@ def gerarListaClasulasSoPodeUm(clasulas):
                 var1 = clasulas[i][j] * -1
                 var2 = clasulas[i][k] * -1
                 listaNovasClasulas.append([var1,var2])
-    
-    addClasulasEmGlucose(listaNovasClasulas)      
+         
     return listaNovasClasulas         
       
-        
+
+# funcao para definir que cada valor precisa assumir algum quadrado
+def gerarListaClasulasValorDeveAssumirUm(dicionario):
+  listaClasulasNovas = []
+  for i in range(VALOR_MAXIMO):
+    auxCriarClasula = []
+    for j in range(TAMANHO_MAXIMO):
+      for k in range(TAMANHO_MAXIMO):
+        auxChaveDic = "1_P_{}_{}_{}".format(j+1,k+1,i)
+        auxObterValor = dicionario[auxChaveDic]
+        auxCriarClasula.append(auxObterValor)    
+    listaClasulasNovas.append(auxCriarClasula)
+  
+  return listaClasulasNovas
+      
+
+
+            
+
+
+
 
             
 
@@ -76,7 +103,7 @@ def gerarListaClasulasSoPodeUm(clasulas):
 
 
 
-# funcao auxiliar
+# funcao auxiliar, apenas representativo
 
 def imprimir_dicionario_linha_a_linha(meu_dicionario):
   """
@@ -102,36 +129,58 @@ def imprimir_dicionario_linha_a_linha(meu_dicionario):
 
 
 
+
+
+
+
 #PARA O PASSO 1
 
-#gera o dicionario passo1
+# gera o dicionario passo1
 dicionarioClasulasPassoUm = gerarDictClasulaVariavel(1)  
 
-#imprimi
+# imprimir 
 imprimir_dicionario_linha_a_linha(dicionarioClasulasPassoUm)
 
-#gerar clasulas que para cada quadrado do 8 puzzle pode ter um valor
-listaPodeTerUmValor = gerarListaClasulasPeloMenosUm(dicionarioClasulasPassoUm)
+# gerar clasulas que para cada quadrado do 8 puzzle deve assumir um valor
+#listaPodeTerUmValor = gerarListaClasulasPeloMenosUm(dicionarioClasulasPassoUm)
 
-#gerar clasulas que para cada quadrado do 8 puzzle so pode ter exclusivamente um valor
-listaPodeSoUmValor = gerarListaClasulasSoPodeUm(listaPodeTerUmValor)
+# gerar clasulas que para cada quadrado do 8 puzzle so pode ter exclusivamente um valor
+#listaPodeSoUmValor = gerarListaClasulasSoPodeUm(listaPodeTerUmValor)
+
+
+# gera clasulas para que no tabuleiro, um valor deva assumir a posicao em algum quadrado
+listaCadaValorAssumeEmTabuleiro = gerarListaClasulasValorDeveAssumirUm(dicionarioClasulasPassoUm)
+
+# gera clasula para que no tabuleiro so exista valoroes unicos
+# (nao repetir o valor 2  em 2 ou mais quadrados por exemplo)
+
+
+
+
+
+
+
+# prints de cada função para verificar
+
+# print("----------------------------------------------------")
+# print("Impimindo as clasulas de pode ter um valor")
+# for aa in listaPodeTerUmValor:
+#    print(aa)
+# print("----------------------------------------------------")
+
+
+# print("----------------------------------------------------")
+# print("Impimindo as clasulas de so pode ter um valor")
+# for aa in listaPodeSoUmValor:
+#    print(aa)
+# print("----------------------------------------------------")
 
 
 print("----------------------------------------------------")
-print("Impimindo as clasulas de so pode ter um valor")
-for aa in listaPodeTerUmValor:
+print("Impimindo as clasulas que cada valor assume um quadrado em tabuleiro")
+for aa in listaCadaValorAssumeEmTabuleiro:
    print(aa)
 print("----------------------------------------------------")
-
-
-print("----------------------------------------------------")
-print("Impimindo as clasulas de so pode ter um valor")
-for aa in listaPodeSoUmValor:
-   print(aa)
-print("----------------------------------------------------")
-
-
-
 
 
 # fomato do git hub para add clasula
